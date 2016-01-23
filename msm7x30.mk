4468b777a18c264612d53caad8a149900e4e737a
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-01-24 00:39:17 +0200

    msm7x30: add initial shim work for camera lib
    Incomplete but a good start.
    
    Based on:
    CyanogenMod/android_device_htc_dlx@becd240730716ed859530e793f549608b950cb72
    
    Change-Id: I17fa40d0501512a784266a3e9967d21e51bf70b1

--- msm7x30.mk
+++ msm7x30.mk
@@ -59,7 +59,9 @@ PRODUCT_COPY_FILES += \
 	$(COMMON_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf
 
 # Camera
-PRODUCT_PACKAGES += camera.msm7x30
+PRODUCT_PACKAGES += \
+	camera.msm7x30 \
+	libshim_camera
 
 # Audio
 PRODUCT_PACKAGES += \
