d3e86fea763171353e5696e0a9aea843fc00025e
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-02-02 18:22:37 +0200

    msm7x30: disable watch dog for image compilation too
    * Still running out of 9.5mins grace period while booting up with apps
      to optimize. Maybe we should just fork the ART repo and fix the damn
      watch dog timeout ourselves. :|
    
    Change-Id: Ie8d08a9a6e9627ea56f056f39187aca1203a8057

--- msm7x30.mk
+++ msm7x30.mk
@@ -121,7 +121,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.dex2oat-filter=balanced
 	dalvik.vm.dex2oat-swap=false \
 	dalvik.vm.dex2oat-flags=--no-watch-dog \
-	dalvik.vm.image-dex2oat-filter=speed
+	dalvik.vm.image-dex2oat-filter=speed \
+	dalvik.vm.image-dex2oat-flags=--no-watch-dog
 
 # For applications to determine if they should turn off specific memory-intensive
 # features that work poorly on low-memory devices.
