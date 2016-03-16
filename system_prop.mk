6f336e6c660c1634e0bb421d78d7c1644cd662fc
kylon <kylonux@gmail.com>
2016-03-16 11:13:41 +0100

    Don't set a default locale

--- system_prop.mk
+++ system_prop.mk
@@ -68,9 +68,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Misc
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.setupwizard.enable_bypass=1 \
-	ro.com.google.locationfeatures=1 \
-	ro.product.locale.language=en \
-	ro.product.locale.region=US
+	ro.com.google.locationfeatures=1
 
 # Disable strict mode
 PRODUCT_PROPERTY_OVERRIDES += \
