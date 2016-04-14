14f5c45eae254a87c14671440779b1f4d3334ff6
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2016-04-14 10:40:40 -0700

    msm7x30: remove BLE permissions
    Change-Id: I58c7d6f1c5bd2196f9e714b05bbb6fba7426c095

--- msm7x30.mk
+++ msm7x30.mk
@@ -36,7 +36,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
