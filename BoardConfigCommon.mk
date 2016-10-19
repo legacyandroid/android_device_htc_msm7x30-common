16390bf20cab13099d5dd98844a8e3ac14289f88
kylon <kylonux@gmail.com>
2016-10-19 17:58:57 +0200

    Correct wifi firmware path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,8 +108,8 @@ BOARD_WLAN_DEVICE                := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 WIFI_BAND                        := 802_11_ABG
-WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
